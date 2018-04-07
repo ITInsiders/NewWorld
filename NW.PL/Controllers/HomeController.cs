7047379852f9512920aea52401a518c8501be32e
Dmitry Vasiliev <ceh4top@gmail.com>
2018-04-07 20:34:10 +0300

    Global

--- NW.PL/Controllers/HomeController.cs
+++ NW.PL/Controllers/HomeController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NW.BL.Services;
 
-namespace NW.Controllers
+namespace NW.PL.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            UserServices.GetAll();
             return View();
         }
 
--- NW.PL/Global.asax
+++ NW.PL/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NW.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NW.PL.MvcApplication" Language="C#" %>
