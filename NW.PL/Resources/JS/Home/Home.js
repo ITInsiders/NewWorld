c8c986ec8fda2fc3a1e3ca5c84df035959a476df
Dmitry Vasiliev <ceh4top@gmail.com>
2018-04-10 02:10:45 +0300

    Scroll

--- NW.PL/Resources/JS/Home/Home.js
+++ NW.PL/Resources/JS/Home/Home.js
@@ -4,7 +4,6 @@ function init() {
     var position = [51.661535, 39.200287];
 
     ymaps.geolocation.get({
-        provider: 'browser'
     }).then(function (result) {
         position = result.geoObjects.position;
 
@@ -18,3 +17,44 @@ function init() {
         controls: []
     });
 }
+
+var scrollPage = true;
+window.addEventListener("mousewheel", function (e) {
+    e = e || window.event;
+    if (scrollPage === true)
+        ScrollPage(e);
+});
+
+function ScrollPage(e) {
+    var $window = $(window),
+        scrollTop = $window.scrollTop();
+    $('html, body').animate({ scrollTop: (scrollTop + ((e.deltaY > 0) ? $window.height() : -$window.height())) }, 500);
+    scrollPage = false;
+    setTimeout(setScrollPage, 500);
+}
+
+function setScrollPage(scroll = true) {
+    scrollPage = scroll;
+}
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 10)
+        $("header").addClass("min");
+    else
+        $("header").removeClass("min");
+});
+$(document).ready(function () {
+    if ($(window).scrollTop() > 10)
+        $("header").addClass("min");
+    else
+        $("header").removeClass("min");
+});
+
+$(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        event.preventDefault();
+        var sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+        $('html, body').animate({ scrollTop: dn }, 500);
+    });
+});
