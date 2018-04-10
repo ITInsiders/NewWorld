fb7c3556f43e50cb17e929313905c3c873e69e7e
Dmitry Vasiliev <ceh4top@gmail.com>
2018-04-10 23:34:17 +0300

    LESS

--- NW.PL/Models/PageInfo.cs
+++ NW.PL/Models/PageInfo.cs
@@ -17,9 +17,15 @@ namespace NW.PL.Models
         public string LayoutHref => "~/Views/Layout/" + Layout + ".cshtml";
 
         public string ControllerStyleHref => "~/Resources/CSS/" + ControllerHref + ".css";
+        public string ControllerStyleMinHref => "~/Resources/CSS/" + ControllerHref + ".min.css";
+        public string ControllerStyleLessHref => "~/Resources/LESS/" + ControllerHref + ".less";
+
         public string ControllerScriptHref => "~/Resources/JS/" + ControllerHref + ".js";
 
         public string ViewStyleHref => "~/Resources/CSS/" + ViewHref + ".css";
+        public string ViewStyleMinHref => "~/Resources/CSS/" + ViewHref + ".min.css";
+        public string ViewStyleLessHref => "~/Resources/LESS/" + ViewHref + ".less";
+
         public string ViewScriptHref => "~/Resources/JS/" + ViewHref + ".js";
 
         public PageInfo(string Controller = null, string View = null)
